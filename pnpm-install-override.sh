e9aebb0229228ae8e9e772ae41701bc90a8ae76d
Paul Hughes <paulus.coding@gmail.com>
2025-12-05 20:25:57 +0000

    Work towards adding overrides from PNPM audit to the package.json file

---
+++ pnpm-install-override.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env zsh
+
+SCRIPT_DIR="${0:A:h}"
+AUDIT_SCRIPT="$SCRIPT_DIR/add-overrides.mjs"
+
+ARGS="$@"
+echo "Removing Any Previous Workspace.yaml File"
+rm -rf pnpm-workspace.yaml
+
+echo "Running pnpm install --lockfile-only"
+pnpm install --lockfile-only $ARGS
+
+echo "Auditing project"
+pnpm audit
+audit_status=$?
+
+if [ $audit_status -ne 0 ]; then
+	pnpm audit fix
+	echo "Adding Overrides to package.json from pnpm-workspace.yaml"
+	node "$AUDIT_SCRIPT"
+fi
